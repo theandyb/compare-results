e9c62aec3e7354ea259e590bdb0f91a910ce8819
Andy Beck <beckandy@umich.edu>
2018-09-19 13:52:49 -0400

    sanity check

--- src/sep_vars.sh
+++ src/sep_vars.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/zsh
 for y in $(tail -n +2 output/AT_CG.csv | cut -f2 -d "," | sort | uniq); do
-	echo $y
+	head -1 output/AT_CG.csv > output/Intercept.csv
 	for x in output/[A,G]*; do
-		echo $x;
+		echo "output/$y.csv"
 	done
 done
     
