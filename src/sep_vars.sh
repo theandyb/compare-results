c157cfcdb594bd46543db17dcb5b65d50d026444
Andy Beck <beckandy@umich.edu>
2018-09-19 14:06:46 -0400

    fixed

--- src/sep_vars.sh
+++ src/sep_vars.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/zsh
 for y in $(tail -n +2 output/AT_CG.csv | cut -f2 -d "," | sort | uniq); do
-	head -1 output/AT_CG.csv > output/Intercept.csv
+	head -1 output/AT_CG.csv > output/feature_specific/${${y:gs/\(/}:gs/\)/}.csv
 	for x in output/[A,G]*; do
-		cat $x | grep '$y' >> "output/${${y:gs/\(/}:gs/\)/}.csv"
+		cat $x | grep $y >> "output/feature_specific/${${y:gs/\(/}:gs/\)/}.csv"
 	done
 done
     
