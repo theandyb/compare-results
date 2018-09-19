19ae8d836e31c1f4521c1100a1b332851976c135
Andy Beck <beckandy@umich.edu>
2018-09-19 14:00:21 -0400

    ready to run!

--- src/sep_vars.sh
+++ src/sep_vars.sh
@@ -2,7 +2,7 @@
 for y in $(tail -n +2 output/AT_CG.csv | cut -f2 -d "," | sort | uniq); do
 	head -1 output/AT_CG.csv > output/Intercept.csv
 	for x in output/[A,G]*; do
-		echo "output/$y.csv"
+		cat $x | grep '$y' >> "output/${${y:gs/\(/}:gs/\)/}.csv"
 	done
 done
     
