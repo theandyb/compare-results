ed08646388752e4606f858ad5e93d78ddfb3aac1
Andy Beck <beckandy@umich.edu>
2018-09-19 13:49:12 -0400

    zsh script now works

--- src/sep_vars.sh
+++ src/sep_vars.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/zsh
-VARS = tail -n +2 output/AT_CG.csv | cut -f2 -d "," | sort | uniq
-for y in $VARS; do echo $y; done
+for y in $(tail -n +2 output/AT_CG.csv | cut -f2 -d "," | sort | uniq); do
+	echo $y
+	for x in output/[A,G]*; do
+		echo $x;
+	done
+done
     
